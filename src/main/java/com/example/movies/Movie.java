c563fefc981713f3195da37437dc3d7e9f8bc628
KoteSepulveda28 <kotesepulveda28@gmail.com>
2024-03-15 23:54:27 -0300

    DSY2201_Exp1_S2_1.1

--- src/main/java/com/example/movies/Movie.java
+++ src/main/java/com/example/movies/Movie.java
@@ -3,19 +3,44 @@ package com.example.movies;
 public class Movie {
     private int idMovie;
     private String nameMovie;
+    private int year;
+    private String director;
+    private String genre;
+    private String synopsis;
 
     // GET METHODS
-    public Movie(int idMovie, String nameMovie){
+    public Movie(int idMovie, String nameMovie, int year, String director, String genre, String synopsis){
         this.idMovie = idMovie;
         this.nameMovie = nameMovie;
+        this.year = year;
+        this.director = director;
+        this.genre = genre;
+        this.synopsis = synopsis;
     }
-
+    
+    // GET METHOD
     public int getIdMovie(){
         return idMovie;
     }
 
     public String getNameMovie(){
         return nameMovie;
+    }
+
+    public int getYearMovie(){
+        return year;
+    }
+    
+    public String getDirectorMovie(){
+        return director;
+    }
+
+    public String getGenreMovie(){
+        return genre;
+    }
+
+    public String getSynopsisMovie(){
+        return synopsis;
     }
 }
 
--- src/main/java/com/example/movies/MovieController.java
+++ src/main/java/com/example/movies/MovieController.java
@@ -15,9 +15,14 @@ public class MovieController {
     private List<Movie> movies = new ArrayList<>();
 
     public MovieController(){
-        movies.add(new Movie(1, "Shrek"));
-        movies.add(new Movie(2, "Shrek 2"));
-        movies.add(new Movie(1, "Shrek 3"));
+        movies.add(new Movie(1, "Shrek", 2001, "Andrew Adamson", "Animación", "Un ogro con un burro"));
+        movies.add(new Movie(2, "The Dark Knight", 2008, "Christopher Nolan", "Acción", "Batman enfrenta al Joker"));
+        movies.add(new Movie(3, "Forrest Gump", 1994, "Robert Zemeckis", "Drama", "La vida de un hombre con discapacidad intelectual"));
+        movies.add(new Movie(4, "Inception", 2010, "Christopher Nolan", "Ciencia ficción", "Robando secretos del subconsciente"));
+        movies.add(new Movie(5, "Pulp Fiction", 1994, "Quentin Tarantino", "Crimen", "Historias entrelazadas en Los Ángeles"));
+        movies.add(new Movie(6, "The Shawshank Redemption", 1994, "Frank Darabont", "Drama", "La vida en una prisión"));
+        movies.add(new Movie(7, "The Lord of the Rings: The Fellowship of the Ring", 2001, "Peter Jackson", "Fantasía", "Una aventura épica"));
+        movies.add(new Movie(8, "The Matrix", 1999, "The Wachowskis", "Ciencia ficción", "La realidad es una simulación"));
     }
 
     @GetMapping("movies")
